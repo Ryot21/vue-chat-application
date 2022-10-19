616212ea55c6eb4cfdaa5cac9498a9ec86c8c417
Ryota Maeda <r.maeda@jaythree.com>
2022-10-20 03:23:10 +0900

    ・HeadBer.vue ⇨ HeadBerComponents.vueへ変更 ・チャットボード、ユーザー一覧、ヘッダー > デザイン変更 ・チャットボード画面 > textarea追加

--- src/components/HeadBerComponents.vue
+++ src/components/HeadBerComponents.vue
@@ -1,18 +1,22 @@
 <template>
-				<header class="l-header c-fixed right c-flex c-flex-01">
+			<header class="l-header c-fixed right c-flex c-flex-01">
 				<!-- <div class="l-icon"></div>
 				<div class="inputArea">
 					<input class="l-login" type="text">
 				</div> -->
+				
 				<h2 class="header-title">
 					ユーザー一覧
 				</h2>
+				<div class="l-avatarArea mg-r10">
+					<div class="c-avatar__small"></div>
+				</div>
 			</header>
 </template>
 
 <script>
 export default {
-  name: 'HeadBer',
+  name: 'HeadBerComponents',
   props: {
     
   }
