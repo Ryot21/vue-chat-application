491cddc0c391d19553b25fb0f4ac11d2f727dd26
Ryota Maeda <r.maeda@jaythree.com>
2022-10-28 01:39:53 +0900

    ・ログイン画面 > デザイン追加

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,27 @@
 <template>
-	<div class="whiteText">ログイン画面</div>
+		<div class="c-modal bg">
+			<div class="modalBlock -login">
+				<div class="modalWrap">
+					<h1 class="loginTitle mg-b20">VueChat</h1>
+					<form action="" >
+						<!-- <input type="text" placeholder="ユーザーネームを入力してください"> -->
+						<div class="l-inputArea__login mg-b10">
+							<input type="text" placeholder="メールアドレスを入力してください">
+						</div>
+						<div class="l-inputArea__login mg-b20">
+							<input type="text" placeholder="パスワードを入力してくだい">
+						</div>
+						<div class="l-btnArea -center mg-b20">
+							<button class="loginBtn">ログイン</button>
+						</div>
+					</form>
+					<div class="loginOrText mg-b20"><span>または</span></div>
+					<div class="l-btnArea -center mg-b20">
+						<button class="entryBtn">新規登録</button>
+					</div>
+				</div>
+			</div>
+		</div>
 </template>
 
 <script>
